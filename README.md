e76a116ffa436171de6f2729d7e0483d34f0a3ac
ManishPillai7 <manishpillaimohit@gmail.com>
2023-08-06 10:30:00 +0530

    Updated

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Function to count the number of files in the current directory Get the correct number of files in the current directory Loop until the user guesses the correct number
+# THE GUSSING GAME
 
-This README.md was generated on Sat Aug  5 20:14:01 IST 2023.
+This README.md was generated on Sun Aug  6 10:24:41 IST 2023.
 
 The number of lines of code in guessinggame.sh is 26.
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# Function to count the number of files in the current directory
+# THE GUSSING GAME
 get_file_count() {
   echo $(ls -1A | wc -l)
 }
 
-# Get the correct number of files in the current directory
+
 correct_count=$(get_file_count)
 
 echo "Welcome to the Guessing Game!"
 echo "Guess the number of files in the current directory:"
 
-# Loop until the user guesses the correct number
+
 while true; do
   read -p "Enter your guess: " guess
 
